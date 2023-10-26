6e9ac7d262e9b30c8f0ae12ff9587b2509297503
Elvis <elvisnipah@gmail.com>
2023-10-26 11:38:30 +0000

    setup env variables and connect to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const config = require("./utils/config");
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => console.log("connected to MongoDB"))
+  .catch((error) =>
+    console.error("error connecting to MongoDB", error.message)
+  );
 
 module.exports = app;
