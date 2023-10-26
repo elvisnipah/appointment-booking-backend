5cf68eaf4acdfae8c977a88239d02b8993e307a0
Elvis <elvisnipah@gmail.com>
2023-10-26 14:11:50 +0000

    install monoogse-unique-validator, add admin model

---
+++ models/admin.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const adminSchema = new mongoose.adminSchema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
+
+adminSchema.plugin(uniqueValidator);
+
+adminSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+module.exports = mongoose.model("Admin", adminSchema);
