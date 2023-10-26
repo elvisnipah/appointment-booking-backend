893e5807f19b255a6058e77ffd900e5fa8cae2f7
Elvis <elvisnipah@gmail.com>
2023-10-26 16:07:50 +0000

    remove ability to post to /admins

--- controllers/admins.js
+++ controllers/admins.js
@@ -7,38 +7,38 @@ adminsRouter.get("/", async (request, response) => {
   response.json(admins);
 });
 
-adminsRouter.post("/", async (request, response) => {
-  const { username, password } = request.body;
+// adminsRouter.post("/", async (request, response) => {
+//   const { username, password } = request.body;
 
-  if (!username || !password) {
-    return response
-      .status(400)
-      .json({ error: "username or password is empty" });
-  }
+//   if (!username || !password) {
+//     return response
+//       .status(400)
+//       .json({ error: "username or password is empty" });
+//   }
 
-  if (password.length < 5) {
-    return response
-      .status(400)
-      .json({ error: "Password must be at least 5 characters." });
-  }
+//   if (password.length < 5) {
+//     return response
+//       .status(400)
+//       .json({ error: "Password must be at least 5 characters." });
+//   }
 
-  if (username.length < 3) {
-    return response
-      .status(400)
-      .json({ error: "Username must be at least 3 characters." });
-  }
+//   if (username.length < 3) {
+//     return response
+//       .status(400)
+//       .json({ error: "Username must be at least 3 characters." });
+//   }
 
-  const saltRounds = 10;
-  const passwordHash = await bcryt.hash(password, saltRounds);
+//   const saltRounds = 10;
+//   const passwordHash = await bcryt.hash(password, saltRounds);
 
-  const admin = new Admin({
-    username,
-    passwordHash,
-  });
+//   const admin = new Admin({
+//     username,
+//     passwordHash,
+//   });
 
-  const savedAdmin = await admin.save();
+//   const savedAdmin = await admin.save();
 
-  response.status(201).json(savedAdmin);
-});
+//   response.status(201).json(savedAdmin);
+// });
 
 module.exports = adminsRouter;
